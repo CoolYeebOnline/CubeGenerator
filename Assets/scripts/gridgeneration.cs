27862456026baaf9d44dad1d8c1ae0b5b04575be
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2022-02-15 13:09:11 +0000

    Rendering and Changes

--- Assets/scripts/gridgeneration.cs
+++ Assets/scripts/gridgeneration.cs
@@ -8,9 +8,10 @@ public class gridgeneration : MonoBehaviour
     public GameObject prefab;
 
     public Color BlankColour, Visible;
-    GameObject Maincube;
-    GameObject child; 
+    //GameObject Maincube;
+    //GameObject child; 
 
+    //Slider values for Grid Size coordinates
     [Range(1,8)]
     public int SizeY;
     [Range(1, 8)]
@@ -18,20 +19,36 @@ public class gridgeneration : MonoBehaviour
     [Range(1, 8)]
     public int SizeZ;
 
+    MaterialPropertyBlock mpb;
+
+    static readonly int shCubeColor = Shader.PropertyToID("_Color");
+    public MaterialPropertyBlock Mpb
+    {
+        get
+        {
+            if (mpb == null)
+                mpb = new MaterialPropertyBlock();
+            return mpb;
+        }
+    }
+    
+
     // Start is called before the first frame update
     void Start()
     {
 
         StartGen();
 
-//        Maincube = gameObject;
-  //      child = Maincube.transform.GetChild(0).gameObject;
+    //      Maincube = gameObject;
+    //      child = Maincube.transform.GetChild(0).gameObject;
     }
     public void StartGen()
     {
 
         Delete();
 
+
+        int CubeCount = 1;
         for (int x = 0; x< SizeX; x++)
         {
             for (int y= 0; y < SizeY; y++)
@@ -43,21 +60,26 @@ public class gridgeneration : MonoBehaviour
                     newGridPiece.transform.localPosition = newPosition;
 
 
-                    newGridPiece.TryGetComponent(out Renderer rend);
+                    newGridPiece.TryGetComponent(out MeshRenderer rend);
 
 
-                    float interp = (float)transform.childCount / (SizeX + SizeZ + SizeY);
-                    Debug.Log(interp);
-                    
-                    if(Application.isEditor)
-                    {
-                        rend.sharedMaterial.color = Color.Lerp(BlankColour, Visible, interp);
+                    //float interp = (float)transform.childCount / (SizeX + SizeZ + SizeY);
+                    //Debug.Log(interp);
 
-                    }
-                    else
-                    {
-                        rend.material.color = Color.Lerp(BlankColour, Visible, interp);
-                    }
+
+                    CubeCount++;
+
+                   if (Application.isEditor)
+                   {
+
+
+                        ApplyColour(CubeCount, rend);
+                   
+                   }
+                   else
+                   {
+                        ApplyColour(CubeCount, rend);
+                   }
 
                 }
             }
@@ -66,6 +88,7 @@ public class gridgeneration : MonoBehaviour
 
     public void Delete()
     {
+        //in editor deletes the child objects of the gameobject to clear the grid (using DestroyImmediate)
         if (Application.isEditor)
         {
 
@@ -82,8 +105,27 @@ public class gridgeneration : MonoBehaviour
         }
         else
         {
+            //does the same but for during runtime (using Destroy)
             foreach (Transform i in transform) Destroy(i.gameObject);
         }
+    }
+
+    public void CubePen()
+    {
+        //to add later
+        //material functionality
+        //double click? / right click? to change cube material
+    }
+
+    void ApplyColour(int cubeNum, MeshRenderer meshRenderer)
+    {
+//        MeshRenderer[] rnd = GetComponentsInChildren<MeshRenderer>();
+        Color MatColor = Color.Lerp(BlankColour, Visible, ((float)cubeNum / transform.childCount));
+
+        Debug.Log(((float)cubeNum /     (SizeX * SizeY * SizeZ)));
+        Mpb.SetColor(shCubeColor, MatColor);
+        meshRenderer.SetPropertyBlock(Mpb);
+        
     }
 
     // Update is called once per frame
